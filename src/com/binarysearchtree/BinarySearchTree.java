8fcc4be6dd190c3cfb9399b2a21c2b8661becc32
Rohit Patro <rohitpatro6@gmail.com>
2021-07-29 20:59:58 +0530

    ADDED:Binary Search Tree Added

--- src/com/binarysearchtree/BinarySearchTree.java
+++ src/com/binarysearchtree/BinarySearchTree.java
@@ -1,7 +1,59 @@
 package com.binarysearchtree;
 
 public class BinarySearchTree {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Binary Search Tree");
+	class Node {
+		int key;
+		Node left;
+		Node right;
+
+		public Node(int item) {
+			key = item;
+			left = right = null;
+		}
+	}
+	Node root;
+	int key;
+
+	public BinarySearchTree() {
+		root = null;
+	}  
+
+	void insert(int key) {
+		root = insertRec(root, key);
+	}
+
+	Node insertRec(Node root,int key) {
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
+		if (key < root.key)
+			root.left = insertRec(root.left,key);
+		else if (key > root.key)
+			root.right = insertRec(root.right, key);
+		return root;
+
+	}
+
+	void inorder() {
+		inorderRec(root);
+	}
+	void inorderRec(Node root)
+	{
+		if (root != null) {
+			inorderRec(root.left);
+			System.out.print( root.key + " ");
+			inorderRec(root.right);
+		}
+	}
+	public static void main(String[] args){
+		BinarySearchTree tree =new BinarySearchTree();
+
+		tree.insert(56);
+		tree.insert(30);
+		tree.insert(70);
+
+		System.out.println("Display the Tree inorder");
+		tree.inorder();
 	}
 }
